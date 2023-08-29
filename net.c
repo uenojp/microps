b85dc3e4a7d777a1c4c42761c0d9f30e51a96c44
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-08-29 15:08:55 +0900

    step18: Implement UDP input/output

--- net.c
+++ net.c
@@ -10,6 +10,7 @@
 #include "icmp.h"
 #include "ip.h"
 #include "platform.h"
+#include "udp.h"
 #include "util.h"
 
 struct net_protocol {
@@ -336,6 +337,11 @@ int net_init(void) {
 
     if (icmp_init() == -1) {
         errorf("icmp_init() failure");
+        return -1;
+    }
+
+    if (udp_init() == -1) {
+        errorf("udp_init() failure");
         return -1;
     }
 
