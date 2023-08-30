39d1022d621587118805605b123b45628af28222
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2023-08-30 11:09:22 +0900

    step22: Implement TCP input

--- net.c
+++ net.c
@@ -9,6 +9,7 @@
 #include "arp.h"
 #include "icmp.h"
 #include "ip.h"
+#include "tcp.h"
 #include "platform.h"
 #include "udp.h"
 #include "util.h"
@@ -380,6 +381,11 @@ int net_init(void) {
 
     if (udp_init() == -1) {
         errorf("udp_init() failure");
+        return -1;
+    }
+
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
         return -1;
     }
 
